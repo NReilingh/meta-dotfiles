da1e442dd1d67c813fa2b8fb025f0783c446cee3
Nick Reilingh <nreilingh@gmail.com>
2024-11-15 19:02:18 -0500

    Fixed broken argv and started e2e tests

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,36 @@
 // import * as cmd from './commands/mod.ts';
 
 import { Command, Options } from '@effect/cli';
-import { Console, Effect } from 'effect';
+import { Console, Effect, Boolean } from 'effect';
 import { BunContext, BunRuntime } from '@effect/platform-bun';
 
 // config.setup();
 
+const helloWorld = Command.make('hello', {}, () => Console.log('Hello, World!'));
+
 const confirm = Options.boolean('confirm').pipe(Options.withAlias('c'));
 const command = Command
   .make('dfi', { confirm }, ({ confirm }) =>
-    Console.log(`Hello this is the dfi command and we are${confirm ? '' : ' NOT'} in confirmation mode.`),
-  );
-  // .pipe(Command.withSubcommands([
-  //   cmd.add,
-  //   cmd.init,
-  //   cmd.inherit,
-  //   cmd.join,
-  //   cmd.merge,
-  //   cmd.sync,
-  // ]));
+    Boolean.match(confirm, {
+      onFalse: () => Console.log("We are running in confirmation mode."),
+      onTrue: () => Console.log("We are not running in confirmation mode.")
+    }))
+  .pipe(Command.withSubcommands([
+    helloWorld,
+    // cmd.add,
+    // cmd.init,
+    // cmd.inherit,
+    // cmd.join,
+    // cmd.merge,
+    // cmd.sync,
+  ]));
 
 const cli = Command.run(command, {
   name: 'meta-dotfiles',
   version: '0.0.1',
 });
 
-Effect.suspend(() => cli(Bun.argv.slice(2))).pipe(
+Effect.suspend(() => cli(Bun.argv)).pipe(
   Effect.provide(BunContext.layer),
   BunRuntime.runMain
 );
