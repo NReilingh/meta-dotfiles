77408cc3ea9ec53cc4fb3ac6f9390514105cbe2f
Nick Reilingh <nreilingh@gmail.com>
2024-11-25 12:41:47 -0500

    Finish rewriting half of tests

--- src/lib/manual.test.ts
+++ src/lib/manual.test.ts
@@ -4,104 +4,36 @@ import { Effect, Context, pipe } from 'effect';
 import { test, expect, describe, mock } from 'bun:test';
 
 function makeMockPrompt (writeMock?: (x: any) => Effect.Effect<string>, readString?: string): Context.Tag.Service<UserPrompt> {
-  return {
+  return UserPrompt.of({
     prompt: writeMock ?? (() => Effect.succeed(readString ?? "")),
-  };
+  });
 }
 
-describe("ConsolePrompt service implementation", () => {
-  test("write calls console.log", async () => {
-    const consoleMock = mock();
-    globalThis.console = {
-      log: consoleMock,
-      async *[Symbol.asyncIterator] () {
-        yield "fooYield";
-      },
-    } as unknown as Console;
-
-    await Effect.runPromise(ConsolePrompt.prompt("hello world"));
-    expect(consoleMock).toHaveBeenCalledWith("hello world");
-  });
-
-  test("read returns console input", async () => {
-    globalThis.console = {
-      async *[Symbol.asyncIterator] () {
-        yield "fooYield";
-      },
-    } as unknown as Console;
-
-    const result = await Effect.runPromise(ConsoleIO.read);
-    expect(result).toBe("fooYield");
-
-    globalThis.console = {
-      async *[Symbol.asyncIterator] () { },
-    } as unknown as Console;
-    const secondResult = await Effect.runPromise(ConsoleIO.read);
-    expect(secondResult).toBe("");
-  });
-});
-
 describe("manualStep", () => {
-  test("returns a function", () => {
-    const result = manualStep({
-      script: [''],
-      outputThunk: () => null,
-    });
-    expect(typeof result).toBe("function");
-  });
-
-  test("return function returns an effect", () => {
-    const step = manualStep({
-      script: [''],
-      outputThunk: () => null,
-    });
-    const result = step(null);
+  test("returns an Effect", () => {
+    const result = manualStep('', () => null);
     expect(result.constructor.name).toBe('EffectPrimitive');
   });
 
-  test("passes input to outputThunk", async () => {
-    const outputMock = mock();
-    const step = manualStep({
-      script: [''],
-      outputThunk: outputMock,
-    });
-    const program = step('helloInputTest')
-      .pipe(Effect.provideService(UserIO, makeMockIO()));
-    await Effect.runPromise(program);
-    expect(outputMock).toHaveBeenCalledWith('helloInputTest', '');
-  });
-
   test("writes prompt message", async () => {
-    const step = manualStep({
-      script: ["What is your favorite color? "],
-      outputThunk: () => null,
-    });
-    const writeMock = mock(() => Effect.sync(() => {}));
-    const program = step(null)
-      .pipe(Effect.provideService(UserIO, makeMockIO(writeMock)));
+    const writeMock = mock(() => Effect.sync(() => ''));
+    const program = manualStep("What is your favorite color?", () => null)
+      .pipe(Effect.provideService(UserPrompt, makeMockPrompt(writeMock)));
     await Effect.runPromise(program);
-    expect(writeMock).toHaveBeenCalledWith("What is your favorite color? ");
+    expect(writeMock).toHaveBeenCalledWith("What is your favorite color?");
   });
 
   test("passes user input to outputThunk", async () => {
     const outputMock = mock();
-    const step = manualStep({
-      script: ["What is your favorite color? "],
-      outputThunk: outputMock,
-    });
-    const program = step(null)
-      .pipe(Effect.provideService(UserIO, makeMockIO(undefined, "blue")));
+    const program = manualStep('', outputMock)
+      .pipe(Effect.provideService(UserPrompt, makeMockPrompt(undefined, "blue")));
     await Effect.runPromise(program);
-    expect(outputMock).toHaveBeenCalledWith(null, "blue");
+    expect(outputMock).toHaveBeenCalledWith("blue");
   });
 
   test("outputThunk result is returned", async () => {
-    const step = manualStep({
-      script: [''],
-      outputThunk: () => "fooBar",
-    });
-    const program = step(null)
-      .pipe(Effect.provideService(UserIO, makeMockIO()));
+    const program = manualStep('', () => "fooBar")
+      .pipe(Effect.provideService(UserPrompt, makeMockPrompt()));
     const result = await Effect.runPromise(program);
     expect(result).toBe("fooBar");
   });
@@ -109,18 +41,18 @@ describe("manualStep", () => {
   test("full send", async () => {
     const program = pipe(
       Effect.succeed(42),
-      Effect.flatMap(manualStep({
-        script: promptScript`What color is this number? ${input => input.toString()}`,
-        outputThunk: (input, value) => ({
+      Effect.flatMap((input) => manualStep(
+        `What color is this number? ${input}`,
+        (value) => ({
           color: value,
           number: input
-        }),
-      })),
+        })
+      )),
     );
 
-    const io = makeMockIO(undefined, "blue");
+    const prompt = makeMockPrompt(undefined, "blue");
     const result = await Effect.runPromise(
-      program.pipe(Effect.provideService(UserIO, io))
+      program.pipe(Effect.provideService(UserPrompt, prompt))
     );
     expect(result).toEqual({
       color: "blue",
@@ -128,4 +60,38 @@ describe("manualStep", () => {
     });
   });
 });
+
+describe("ConsolePrompt service implementation", () => {
+  function makeMockConsole (
+    logMock?: (x: any) => void,
+    readString?: string
+  ): void {
+    globalThis.console = {
+      log: logMock ?? (() => {}),
+      async *[Symbol.asyncIterator] () {
+        yield readString ?? "";
+      },
+    } as unknown as Console;
+  }
+  test("write calls console.log", async () => {
+    const consoleMock = mock();
+    makeMockConsole(consoleMock);
+
+    await Effect.runPromise(ConsolePrompt.prompt("hello world"));
+    expect(consoleMock).toHaveBeenCalledWith("hello world");
+  });
+
+  test("read returns console input", async () => {
+    makeMockConsole(undefined, "fooYield");
+
+    const result = await Effect.runPromise(ConsolePrompt.prompt(''));
+    expect(result).toBe("fooYield");
+
+    makeMockConsole();
+
+    const secondResult = await Effect.runPromise(ConsolePrompt.prompt(''));
+    expect(secondResult).toBe('');
+  });
+});
+
 
--- src/lib/manual.ts
+++ src/lib/manual.ts
@@ -1,4 +1,4 @@
-import { Effect, Context, Layer, pipe } from 'effect';
+import { Effect, Context, Layer } from 'effect';
 import { Prompt } from '@effect/cli';
 import { Terminal, QuitException } from '@effect/platform/Terminal';
 import { Error } from '@effect/platform';
@@ -16,6 +16,51 @@ export class UserPrompt extends Context.Tag("UserPromptService")<
     readonly prompt: (query: string) => Effect.Effect<string, never | QuitException | Error.PlatformError>,
   }
 >() {}
+
+/**
+ * An effectful function that can be passed to `Effect.flatMap`
+ * to prompt the operator to perform a manual step.
+ *
+ * This should be used differently from `@ffect/cli/Prompt` in that
+ * a `manualStep` should eventually be replaced with an automated solution.
+ *
+ * `manualStep`'s implementation may end up using Prompt
+ * in interactive CLI contexts,
+ * but could also be a web frontend or Slack message.
+ *
+ * Prompt should still be used when the user's participation is always desired.
+ * However, ideally this will be accomplished using Args/Opts with Prompt fallbacks.
+ *
+ * It's entirely possible that this outputThunk pattern is trivially replacable
+ * with a combinator using the UserPrompt service directly.
+ *
+ * I guess it's just Effect.Effect.pipe after all?
+ * The function just gets use the convenience of UserPrompt
+ * without so much ceremony.
+ *
+ * @example
+ * const program = pipe(
+ *   Effect.succeed(42),
+ *   Effect.flatMap((input) => manualStep(
+ *     `What color is this number? ${input}`,
+ *     (value) => ({
+ *       color: value,
+ *       number: input
+ *     }),
+ *   )),
+ * );
+ */
+export function manualStep<TOutput> (
+  script: string,
+  outputThunk?: (value: string) => TOutput,
+) {
+  return UserPrompt.pipe(
+    Effect.flatMap(({ prompt }) => prompt(script)),
+    Effect.map(value => outputThunk ? outputThunk(value) : value),
+  );
+}
+
+// Some "batteries included" implementations of UserPrompt
 
 /**
  * An implementation of UserPrompt using the `@effect/cli/Prompt` library.
@@ -55,41 +100,3 @@ export const TerminalPrompt = Layer.effect(
     })),
   )
 );
-
-/**
- * An effectful function that can be passed to `Effect.flatMap`
- * to prompt the operator to perform a manual step.
- *
- * This should be used differently from `@ffect/cli/Prompt` in that
- * a `manualStep` should eventually be replaced with an automated solution.
- *
- * `manualStep`'s implementation may end up using Prompt
- * in interactive CLI contexts,
- * but could also be a web frontend or Slack message.
- *
- * Prompt should still be used when the user's participation is always desired.
- * However, ideally this will be accomplished using Args/Opts with Prompt fallbacks.
- *
- * @example
- * const program = pipe(
- *   Effect.succeed(42),
- *   Effect.flatMap((input) => manualStep({
- *     script: `What color is this number? ${input}`,
- *     outputThunk: (value) => ({
- *       color: value,
- *       number: input
- *     }),
- *   })),
- * );
- */
-export function manualStep<TOutput> (
-  { script, outputThunk }: {
-    script: string,
-    outputThunk: (value: string) => TOutput,
-  }
-) {
-  return UserPrompt.pipe(
-    Effect.flatMap(({ prompt }) => prompt(script)),
-    Effect.map(value => outputThunk(value)),
-  );
-}
